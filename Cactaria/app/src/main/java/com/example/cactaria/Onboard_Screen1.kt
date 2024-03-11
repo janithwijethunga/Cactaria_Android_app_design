b1c5c741d2ff68db1904731783bace3bc8564296
janithwijethunga <janithwijethunga68@gmail.com>
2024-03-11 11:58:23 +0530

    design pages
    landing page, homescreen, user profile, item page

--- Cactaria/app/src/main/java/com/example/cactaria/Onboard_Screen1.kt
+++ Cactaria/app/src/main/java/com/example/cactaria/Onboard_Screen1.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class Onboard_Screen1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +16,8 @@ class Onboard_Screen1 : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
     }
 }
