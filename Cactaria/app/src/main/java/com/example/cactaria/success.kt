f2c0e50cea0b2b78cecee0f004a1d98d7a4d1076
janithwijethunga <janithwijethunga68@gmail.com>
2024-03-14 02:19:50 +0530

    success page and backend

--- Cactaria/app/src/main/java/com/example/cactaria/success.kt
+++ Cactaria/app/src/main/java/com/example/cactaria/success.kt
@@ -1,31 +1,20 @@
 package com.example.cactaria
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.Toast
 
-class Booknow : AppCompatActivity() {
+class success : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_booknow)
+        setContentView(R.layout.activity_success)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val startedBtn: Button = findViewById(R.id.main)
-        startedBtn.setOnClickListener {
-            Toast.makeText(this, "Order Successful", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, success::class.java)
-            startActivity(intent)
-        }
     }
-
 }
